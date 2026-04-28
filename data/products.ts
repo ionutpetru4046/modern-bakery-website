78ff7e758d59466d3d6f7208b5eaac92ce449111
ionutpetru4046 <ionut4046@yahoo.com>
2026-04-28 19:20:07 +0100

    more products added to data

--- data/products.ts
+++ data/products.ts
@@ -26,4 +26,31 @@ export const products = [
       description:
         "Slow-fermented artisan bread with crispy crust and soft interior.",
     },
+    {
+        id: "4",
+        name: "Chocolate Croissant",
+        price: 8,
+        image:
+          "https://images.unsplash.com/photo-1555507036-ab1f4038808a?q=80&w=1200&auto=format&fit=crop",
+        description:
+          "Buttery, flaky croissant filled with rich Belgian chocolate.",
+      },
+      {
+        id: "5",
+        name: "Strawberry Cake",
+        price: 14,
+        image:
+          "https://images.unsplash.com/photo-1578985545062-69928b1d9587?q=80&w=1200&auto=format&fit=crop",
+        description:
+          "Soft vanilla sponge layered with fresh strawberries and cream.",
+      },
+      {
+        id: "6",
+        name: "Artisan Bread",
+        price: 10,
+        image:
+          "https://images.unsplash.com/photo-1509440159596-0249088772ff?q=80&w=1200&auto=format&fit=crop",
+        description:
+          "Slow-fermented artisan bread with crispy crust and soft interior.",
+      },
   ];
